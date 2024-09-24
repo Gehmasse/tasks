413ed42c2a33b0dfaaef8971b12467adde2b11ac
Leo Nickl <leo@leonickl.de>
2024-09-24 21:56:02 +0200

    supply more information

--- app/Exceptions/CalDavException.php
+++ app/Exceptions/CalDavException.php
@@ -4,4 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 
-class CalDavException extends Exception {}
+class CalDavException extends Exception {
+    public function __construct(string $response, string $ical)
+    {
+        parent::__construct($response . ' - ' . $ical);
+    }
+}
